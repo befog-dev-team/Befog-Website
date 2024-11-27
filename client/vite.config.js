1ffdcf6892dd0599052aa8d268bfd0d79d078244
vinay2003 <vinaysharma31681@gmail.com>
2024-11-27 17:10:06 +0530

    Add profile pop-up, help section for shop

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: './', // Adjust if your files are in a subdirectory
+  build: {
+    outDir: 'dist', // Default output directory
+    rollupOptions: {
+      input: './index.html', // Ensure the path is correct
+    },
+  },
+});
